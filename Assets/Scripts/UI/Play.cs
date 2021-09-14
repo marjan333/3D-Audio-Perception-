dd1c16e7a4483d4301c4e373b860c207a00f6e93
marjan333 <37725791+marjan333@users.noreply.github.com>
2021-09-14 13:28:45 +1000

    NightLighting

---
+++ Assets/Scripts/UI/Play.cs
@@ -0,0 +1,24 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// //using UnityEngine.SceneManagement; 
+// using UnityEngine.UI; 
+
+
+// public class Play : MonoBehaviour
+// {
+//     public Button play; 
+//     void Start(){
+//         Button btn = play.GetComponent<Button>();
+//         btn.onClick.AddListener(play);
+//     }
+
+//     public void PlayGame(){
+//        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0); 
+//        //SceneManager.LoadScene();
+//        //Scene.Play(); 
+//        Application.LoadLevel(0);
+
+//     }
+    
+// }
